bfaf72fac84afd5993643ef5a4e6a937d63fd0d8
zrodriguez <zrodriguez@hiberus.com>
2024-10-18 12:36:07 +0200

    Use Liquibase for database DDL scripts.

--- src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO TBL_SPACESHIPS(spaceship_name) VALUES
-       ('Cygnus Explorer'), ('Serenity'), ('USS Enterprise'), ('TARDIS'), ('Battlestar Galactica'),
-       ('Heart of Gold'), ('Rocinante'), ('Starbug'), ('Red Dwarf'), ('Eagle Transporter'), ('Viper'),
-       ('X-Wing'), ('TIE Fighter'), ('Sulaco'), ('Nostromo'), ('Event Horizon'), ('Starfury'), ('Millenium Falcon');
